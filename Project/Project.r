852b9039c88a7b31fcb47e93e5fb903df69968b5
kingcalyb123 <calybking@iMac.local>
2021-03-04 13:58:49 -0500

    kingcalyb123 Project

---
+++ Project/Project.r
@@ -0,0 +1,6 @@
+setwd('~/Desktop/Evolution/Project/Project')
+Data <- read.csv('Data.csv', stringsAsFactors=F)
+
+Hypothesis:
+
+In warmer temperatures, such as spring and summer, Drosophilia melanogaster will be more resistant to bacterial load and infection. This will be due to climate changes (rises) and evolutionary processes in the subjects immune system.
